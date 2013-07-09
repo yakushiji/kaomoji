6bd0c6e8874dd0069a70378f9c9dde3eaed62d53
yakushiji <yaoshisizhenghe@gmail.com>
2013-07-09 17:18:31 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaomoji::Application.config.secret_token = '82dafef9bcbb3474864d4cbecaff30091da50136a480d4c9d241d1f9e09e9d9dda7412ce7fcbc2f40a1332386331c66f7200f174f5bd0e8fcdb711de68539263'
